185b022f5d699cdc711c84cd7c3e34b7d6f04b05
nohh-a <nrod5432@hotmail.com>
2025-09-10 15:32:01 +1000

    Add SkillsSection showing my skills

---
+++ src/components/SkillsSection.jsx
@@ -0,0 +1,93 @@
+/**
+ *  Page displays my technical skills grouped into categories - showing their proficiency level and can be filtered
+ */
+
+import { useState } from "react";
+import { cn } from "@/lib/utils";
+
+/**
+ * Array of skills to be used with the categories arrray
+ */
+const skills = [
+  { name: "HTML/CSS", level: 95, category: "frontend" },
+  { name: "JavaScript", level: 90, category: "frontend" },
+  { name: "React", level: 70, category: "frontend" },
+  { name: "Tailwind CSS", level: 90, category: "frontend" },
+  { name: "Next.js", level: 90, category: "frontend" },
+
+  { name: "Node.js", level: 90, category: "backend" },
+  { name: "Express", level: 90, category: "backend" },
+  { name: "MongoDB", level: 90, category: "backend" },
+  { name: "PostgreSQL", level: 90, category: "backend" },
+  { name: "GraphQL", level: 90, category: "backend" },
+
+  { name: "Git/GitHub", level: 90, category: "tools" },
+  { name: "Figma", level: 90, category: "tools" },
+  { name: "VS Code", level: 90, category: "tools" },
+  { name: "CPanel", level: 90, category: "tools" },
+  { name: "AWS", level: 90, category: "tools" },
+];
+
+const categories = ["all", "frontend", "backend", "tools"];
+
+export const SkillsSection = () => {
+  // state tracks which filter is active
+  const [activeCategory, setActiveCategory] = useState("all");
+  // .filter loops through item time in the skills array and return a new array depending on what the activy category is
+  const filteredSkills = skills.filter(
+    (skill) => activeCategory === "all" || skill.category === activeCategory
+  );
+  return (
+    <section id="skills" className="py-24 px-4 relative bg-secondar/30">
+      <div className="container mx-auto max-w-5xl">
+        <h2 className="text-3xl md:text-4xl font-bold mb-12 text-center">
+          My <span className="text-primary"> Skills</span>
+        </h2>
+        <div className="flex flex-wrap justify-center gap-4 mb-12">
+          {categories.map((category, key) => (
+            <button
+              key={key}
+              onClick={() => setActiveCategory(category)}
+              /**
+               * combines the first line of tailwind classes as a 'base' class,
+               * then conditional classes if true apply ? else apply :
+               *  */
+              className={cn(
+                "px-5 py-2 rounded-full transition-colors duration-300 capitalize",
+                activeCategory === category
+                  ? "bg-primary text-primary-foreground"
+                  : "bg-secondary/70 text-foreground hover:bg-secondary"
+              )}
+            >
+              {category}
+            </button>
+          ))}
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          {filteredSkills.map((skill, key) => (
+            <div
+              key={key}
+              className="bg-card p-6 rounded-lg shadow-xs card-hover"
+            >
+              {" "}
+              <div className="text-left mb-4">
+                <h3 className="font-semibold text-lg">{skill.name}</h3>
+              </div>
+              <div className="w-full bg-secondary/50 h-2 rounded-full overflow-hidden">
+                <div
+                  className="bg-primary h-2 roudned-full origin-left animate-[grow_1.5s_ease-out"
+                  style={{ width: skill.level + "%" }}
+                />
+              </div>
+              <div className="text-right mt-1">
+                <span className="text-sm text-muted-foreground">
+                  {skill.level}%
+                </span>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
